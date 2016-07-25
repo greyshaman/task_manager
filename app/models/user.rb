0f67b89b56b6c3ff8bdba4588198464e4f9313a3
Sergey Reshetnikov <shaman@simmirra.com>
2016-07-25 23:14:21 +0300

    Added validates and encrypting password callback

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,14 @@
 class User < ActiveRecord::Base
+
+  # Validators
+  validates :email, uniqueness: true
+
+  attr_writer :password
+
+  # Callbacks
+  before_create :encrypt_password
+
+  def encrypt_password
+    self.encrypted_password = BCrypt::Password.create(@password).to_s
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -7,5 +7,11 @@ RSpec.describe User, type: :model do
     it {expect(subject).to have_db_column(:role).of_type(:string).with_options(null: false, default: 'USER')}
 
     it {expect(subject).to have_db_index(:email).unique(true)}
+
+  end
+
+  context "validation" do
+    subject {FactoryGirl.create(:user)}
+    it {expect(subject).to validate_uniqueness_of(:email)}
   end
 end
