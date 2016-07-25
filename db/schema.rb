fa54195014e9094c59a317922607e06c45556627
Sergey Reshetnikov <shaman@simmirra.com>
2016-07-25 22:18:17 +0300

    added index for users email attrinute

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160725190522) do
+ActiveRecord::Schema.define(version: 20160725191555) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,5 +33,7 @@ ActiveRecord::Schema.define(version: 20160725190522) do
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
   end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
 
 end
