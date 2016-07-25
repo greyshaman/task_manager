d0a18db5f7eccf1e8281ba07713d306b19e047c1
Sergey Reshetnikov <shaman@simmirra.com>
2016-07-25 21:15:52 +0300

    Added simplecov

--- Gemfile
+++ Gemfile
@@ -48,6 +48,7 @@ group :development, :test do
 
   gem 'rspec-rails', '~> 3.5'
   gem 'factory_girl_rails', '~> 4.0'
+  gem 'simplecov', :require => false, :group => :test
 end
 
 group :development do
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -14,6 +14,8 @@
 #
 # The `.rspec` file also contains a few flags that are not defaults but that
 # users commonly want.
+require 'simplecov'
+SimpleCov.start
 #
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
 RSpec.configure do |config|
