6c50a70b0af76b19d8c0b861994334ef23c27342
Sergey Reshetnikov <shaman@simmirra.com>
2016-07-28 22:19:16 +0300

    Added presence validator for name of Task

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,7 @@
 class Task < ActiveRecord::Base
   # Associations
   belongs_to :user
+
+  # Validators
+  validates :name, presence: true
 end
--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -13,4 +13,8 @@ RSpec.describe Task, type: :model do
   context "associations" do
     it {is_expected.to belong_to(:user)}
   end
+
+  context "validations" do
+    it {is_expected.to validate_presence_of(:name)}
+  end
 end
