93ce2ef4a330154bde58a91f5f8cb623ee4a0b8a
Sergey Reshetnikov <shaman@simmirra.com>
2016-07-28 21:43:47 +0300

    added Task attributes validations on db layer

--- db/migrate/20160728183636_create_tasks.rb
+++ db/migrate/20160728183636_create_tasks.rb
@@ -1,10 +1,10 @@
 class CreateTasks < ActiveRecord::Migration
   def change
     create_table :tasks do |t|
-      t.string :name
+      t.string :name, null: false
       t.text :description
-      t.integer :user_id
-      t.string :state
+      t.integer :user_id, null: false
+      t.string :state, null: false, default: "new"
 
       t.timestamps null: false
     end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160725191555) do
+ActiveRecord::Schema.define(version: 20160728183636) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,15 @@ ActiveRecord::Schema.define(version: 20160725191555) do
 
   add_index "sessions", ["session_id"], name: "index_sessions_on_session_id", unique: true, using: :btree
   add_index "sessions", ["updated_at"], name: "index_sessions_on_updated_at", using: :btree
+
+  create_table "tasks", force: :cascade do |t|
+    t.string   "name",                        null: false
+    t.text     "description"
+    t.integer  "user_id",                     null: false
+    t.string   "state",       default: "new", null: false
+    t.datetime "created_at",                  null: false
+    t.datetime "updated_at",                  null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                               null: false
