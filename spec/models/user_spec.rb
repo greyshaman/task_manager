3120e39157d95bc5beda8a5624c297cb6fef1ba0
Sergey Reshetnikov <shaman@simmirra.com>
2016-07-28 21:50:23 +0300

    using is_expected instead of expect(subject)

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,11 +2,11 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   context "attributes" do
-    it {expect(subject).to have_db_column(:email).of_type(:string).with_options(null: false)}
-    it {expect(subject).to have_db_column(:encrypted_password).of_type(:string).with_options(null: false)}
-    it {expect(subject).to have_db_column(:role).of_type(:string).with_options(null: false, default: 'USER')}
+    it {is_expected.to have_db_column(:email).of_type(:string).with_options(null: false)}
+    it {is_expected.to have_db_column(:encrypted_password).of_type(:string).with_options(null: false)}
+    it {is_expected.to have_db_column(:role).of_type(:string).with_options(null: false, default: 'USER')}
 
-    it {expect(subject).to have_db_index(:email).unique(true)}
+    it {is_expected.to have_db_index(:email).unique(true)}
 
   end
 
