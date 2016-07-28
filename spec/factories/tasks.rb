030914f6c932934abd7883ea675cc4502f0d32d2
Sergey Reshetnikov <shaman@simmirra.com>
2016-07-28 21:44:18 +0300

    fixed Task factory

--- spec/factories/tasks.rb
+++ spec/factories/tasks.rb
@@ -1,8 +1,15 @@
 FactoryGirl.define do
   factory :task do
-    name "MyString"
-    description "MyText"
-    user_id 1
-    state "MyString"
+    name "Weak Up"
+    user
+    state "new"
+
+    trait :started do
+      state "started"
+    end
+
+    trait :finished do
+      state "finished"
+    end
   end
 end
