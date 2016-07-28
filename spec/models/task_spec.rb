714c061df4f15410a02e320aeed054e20fc58824
Sergey Reshetnikov <shaman@simmirra.com>
2016-07-28 21:56:18 +0300

    Added db attributes tests for Task

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe Task, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context "attributes" do
+    it {is_expected.to have_db_column(:name).of_type(:string).with_options(null: false)}
+    it {is_expected.to have_db_column(:description).of_type(:text)}
+    it {is_expected.to have_db_column(:user_id).of_type(:integer).with_options(null: false)}
+    it {is_expected.to have_db_column(:state).of_type(:string).with_options(null: false, default: 'new')}
+    it {is_expected.to have_db_column(:started_at).of_type(:datetime)}
+    it {is_expected.to have_db_column(:finished_at).of_type(:datetime)}
+  end
 end
