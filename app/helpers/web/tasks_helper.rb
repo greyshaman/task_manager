02560d40548b558b9e9018dee5b0ce3234cc06c5
Sergey Reshetnikov <shaman@simmirra.com>
2016-08-02 05:23:22 +0300

    catch exception from production

--- app/helpers/web/tasks_helper.rb
+++ app/helpers/web/tasks_helper.rb
@@ -1,13 +1,10 @@
 module Web::TasksHelper
   def show_attachment
-    if @task.attachment.present? && @task.attachment_url.present?
-       if @task.attachment.try(:content_type).try(:match, /image/i)
-          image_tag(@task.attachment_url)
-        else
-          link_to(@task.attachment.model[:attachment], @task.attachment_url, target: '_blank')
-        end
+    if @task.attachment.try(:content_type).try(:match, /image/i)
+      image_tag(@task.attachment_url)
     else
-      ""
+      link_to(@task.attachment.model[:attachment], @task.attachment_url, target: '_blank')
     end
+  rescue ""
   end
 end
